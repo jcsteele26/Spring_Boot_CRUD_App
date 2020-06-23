2618dd3d066dab5ab09ba184e54b824552bb457f
Andre <jcsteele26@gmail.com>
2020-06-23 07:41:49 -0400

    Edit java version

--- spring-boot-starter-parent/src/main/java/com/steele/springbootstarterparent/user/UserController.java
+++ spring-boot-starter-parent/src/main/java/com/steele/springbootstarterparent/user/UserController.java
@@ -7,13 +7,16 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.beans.factory.annotation.Autowired;
+
 
 @Controller
 public class UserController {
 
+    @Autowired
     private UserRepository userRepository;
 
-    @Autowired
+    
     public UserController(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
